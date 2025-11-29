4607979c58c7ab3d562c79ea879f951d00d80b61
“[firstname <g.adonoo@yahoo.it>
2025-11-29 23:33:25 +0100

    modify 29112025 23:33

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,14 @@
 class HelloWorld:
-    def __init__(self, hello):
+    def __init__(self, hello, world):
         self.hello = hello
+        self.world = world
 
     def print_hello(self):
         print('Hello..', self.hello)
 
-seyHello = HelloWorld('World!')
+    def print_world(self):
+        print('World..', self.world)
+
+seyHello = HelloWorld('Hello!', 'World!')
 seyHello.print_hello()
+seyHello.print_world()
