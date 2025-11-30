8d2f155c28ea157bb39c7446cb367e5815b63e7a
“[firstname <g.adonoo@yahoo.it>
2025-11-30 01:35:01 +0100

    print

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,8 +1,13 @@
 class NeuralNetwork:
     def __ini__ (self, hidden_layer_size = 100):
         self.hidden_layer_size = hidden_layer_size
-       # print('hidden_layer_size: ', self.hidden_layer_size)
-        return self.hidden_layer_size
+
+    def func(self):
+        print('hidden_layer_size:')
+        #return self.hidden_layer_size
+
+nn = NeuralNetwork()
+nn.func()
 
 
 
