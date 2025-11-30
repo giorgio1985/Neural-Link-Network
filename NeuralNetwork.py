563659036f6a63a392a45e16538eaf9bd212dfc7
“[firstname <g.adonoo@yahoo.it>
2025-11-30 02:37:56 +0100

    add logLoss function

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,12 +1,13 @@
 import numpy as np
 class NeuralNetwork:
-    def __ini__ (self, hidden_layer_size = 100):
+    def __ini__ (self, hidden_layer_size = 100): # Nodi interni all'interno del costruttore
         self.hidden_layer_size = hidden_layer_size
 
-    def _accuracy(self, y, y_pred):
+    def _accuracy(self, y, y_pred):   # frequenza di errore
         return np.sum(y==y_pred)/len(y)
 
-
+    def _log_loss(self, y_true, y_proba):  # Funzione entropica dell'errore
+        return -np.sum(np.dot(y_true,np.log(y_proba))+np.dot((1-y_true),np.log(1-y_proba)))/len(y_true)
     
 
 
