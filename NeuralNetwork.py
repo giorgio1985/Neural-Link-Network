99b3c17102fea67791001746b75b20311304b31d
“[firstname <g.adonoo@yahoo.it>
2025-11-30 02:12:38 +0100

    add accuracy

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,13 +1,15 @@
+import numpy as np
 class NeuralNetwork:
     def __ini__ (self, hidden_layer_size = 100):
         self.hidden_layer_size = hidden_layer_size
 
-    def func(self):
-        print('hidden_layer_size:')
-        #return self.hidden_layer_size
+    def _accuracy(self, y, y_pred):
+        return np.sum(y==y_pred)/len(y)
 
-nn = NeuralNetwork()
-nn.func()
+
+    
+
+
 
 
 
