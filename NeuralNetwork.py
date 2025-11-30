9aa3af2809a39b464e0117e9ff6561bf49c90b3d
“[firstname <g.adonoo@yahoo.it>
2025-11-30 04:19:50 +0100

    Add sigmoid function

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -9,9 +9,11 @@ class NeuralNetwork:
     def _log_loss(self, y_true, y_proba): #Funzione entropica, piu precisa dell'accuracy. Calcola il prodotto scalare.
         return -np.sum(np.dot(y_true,np.log(y_proba))+np.dot((1-y_true),np.log(1-y_proba)))/len(y_true)
 
-    def _relu(self, Z): #Funzione di attivazione
+    def _relu(self, Z): #Funzione di attivazione per strati nascosti
        return np.maximum(Z, 0)
     
+    def _sigmoid(self, Z): #Funzione di attivazione per strati output
+       return 1/(1+np.power(np.e,-Z))
 
 
 
